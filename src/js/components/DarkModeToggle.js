687b05badcb981adb858a5c935cafdd603541701
Junwoo <dkssud0112@naver.com>
2022-03-02 04:34:41 +0900

    중간 저장

--- src/js/components/DarkModeToggle.js
+++ src/js/components/DarkModeToggle.js
@@ -1,43 +1,21 @@
 export default class DarkModeToggle {
   
     constructor({ $target }) {
-      this.isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    
       const $darkModeToggle = document.createElement("input");
       $darkModeToggle.className = "DarkModeToggle";
       $darkModeToggle.type = "checkbox";
-      $darkModeToggle.textContent = "다크모드 활성화";
       this.$darkModeToggle = $darkModeToggle;
+      this.isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
       $target.appendChild($darkModeToggle);
   
-    //   this.render();
+      this.render();
     }
-  
-    // setState(nextData) {
-    //   this.data = nextData;
-    //   this.render();
-    // }
-  
-    // render() {
-    //   if (this.data.visible) {
-    //     const { name, url, temperament, origin } = this.data.image;
-  
-    //     this.$imageInfo.innerHTML = `
-    //       <div class="content-wrapper">
-    //         <div class="title">
-    //           <span>${name}</span>
-    //           <div class="close">x</div>
-    //         </div>
-    //         <img src="${url}" alt="${name}"/>        
-    //         <div class="description">
-    //           <div>성격: ${temperament}</div>
-    //           <div>태생: ${origin}</div>
-    //         </div>
-    //       </div>`;
-    //     this.$imageInfo.style.display = "block";
-    //   } else {
-    //     this.$imageInfo.style.display = "none";
-    //   }
-    // }
+
+    setState() {}
+
+    render() {
+        
+    }
   }
   
